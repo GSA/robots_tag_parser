00e03a16e870dee9a6313d83cdec9573205b01ec
Moth <Moth@Moths-MBP.hsd1.wa.comcast.net>
2017-09-14 13:39:09 -0700

    Revert "failing test"
    This reverts commit f3069b5cf414c048bf0a086cf64512335ae37f30.

--- spec/robots_tag_parser_spec.rb
+++ spec/robots_tag_parser_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe RobotsTagParser do
       let(:headers) { { 'X-Robots-Tag' => 'nofollow' } }
 
       it 'is not case-sensitive' do
-        expect(get_rules).to eq 'foo'#['nofollow']
+        expect(get_rules).to eq ['nofollow']
       end
     end
 
